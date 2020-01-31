7de41d2ac5af5ce0398c6ffd358be4b6f89a978a
pArtour <edvardi72@gmail.com>
2020-01-31 09:52:37 +0200

    Парсировка карточек товаров с помощью fetch api

--- script/script.js
+++ script/script.js
@@ -6,47 +6,68 @@ document.addEventListener('DOMContentLoaded',  () => {
         goodsWrapper = document.querySelector('.goods-wrapper'),
         cart = document.querySelector('.cart');
 
-  const createCardGoods = (id, title, price, img) => {
 
-    const card = document.createElement('div');
-    card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
-    card.innerHTML = `<div class="card">
+        
+        
+        const createCardGoods = (id, title, price, img) => {
+          
+          const card = document.createElement('div');
+          card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+          card.innerHTML = `<div class="card">
                         <div class="card-img-wrapper">
-                          <img class="card-img-top" src="${img}" alt="">
+                        <img class="card-img-top" src="${img}" alt="">
                           <button class="card-add-wishlist" data-goods-id="${id}"></button>
                         </div>
                         <div class="card-body justify-content-between">
-                          <a href="#" class="card-title">${title}</a>
-                          <div class="card-price">${price} ₽</div>
+                        <a href="#" class="card-title">${title}</a>
+                        <div class="card-price">${price} ₽</div>
                           <div>
-                            <button class="card-add-cart" data-goods-id="${id}">Добавить в корзину</button>
+                          <button class="card-add-cart" data-goods-id="${id}">Добавить в корзину</button>
                           </div>
                         </div>
-                      </div>`;
+                        </div>`;
                       
     return card;
-
+                        
   };
   
-  goodsWrapper.appendChild(createCardGoods(1, 'Дартс', 2000, 'img/temp/Archer.jpg'));
-  goodsWrapper.appendChild(createCardGoods(2, 'Фламинго', 3000, 'img/temp/flamingo.jpg'));
-  goodsWrapper.appendChild(createCardGoods(3, 'Носки', 333, 'img/temp/Socks.jpg'));
+  const renderCard = goods => {
+    goodsWrapper.textContent = '';
+    goods.forEach(({id, title, price, imgMin}) => {
+      goodsWrapper.appendChild(createCardGoods(id, title, price, imgMin));
+    });
+  };
 
-
-  const closeCard = (event) => {
+  const closeCard = event => {
     const target = event.target;
-
-    if (target === cart || target.classList.contains('cart-close')) {
+    if (target === cart || target.classList.contains('cart-close') || event.keyCode === 27) {
       cart.style.display = '';  
+      document.removeEventListener('keyup', closeCard);
     }
-
   };
-
-  const openCart = () => {
+  
+  const openCart = event => {
+    event.preventDefault();
     cart.style.display = 'flex';
+    document.addEventListener('keyup', closeCard);
   };
+  
+  const getGoods = (handler, filter) => {
+    fetch('db/db.json')
+        .then(responce => responce.json())
+        .then(filter)
+        .then(handler)
+  };
+  
+  //Рандомная ортировка
+  const randomSort = item => {
+    return item.sort(() => Math.random() - 0.5);
+  }
+
+
 
   cartBtn.addEventListener('click', openCart);
-  cart.addEventListener('click', closeCard)
-
+  cart.addEventListener('click', closeCard);
+  
+  getGoods(renderCard, randomSort);
 });
